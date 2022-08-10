000dacd2b453f62871ff1383d0d7a9f7fd71cce9
Kadhirvel <Kadhirvel.n@zensar.com>
2022-08-10 23:28:45 +0530

    added condition for IOS 13

--- UIUtilityComponents/UIUtilityComponents/Library/CustomAlertDialog.swift
+++ UIUtilityComponents/UIUtilityComponents/Library/CustomAlertDialog.swift
@@ -78,7 +78,11 @@ extension UIViewController{
         let alert = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
         let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
         loadingIndicator.hidesWhenStopped = true
-        loadingIndicator.style = UIActivityIndicatorView.Style.medium
+        if #available(iOS 13.0, *) {
+            loadingIndicator.style = UIActivityIndicatorView.Style.medium
+        } else {
+            // Fallback on earlier versions
+        }
         loadingIndicator.startAnimating();
         alert.view.addSubview(loadingIndicator)
         present(alert, animated: true, completion: nil)
